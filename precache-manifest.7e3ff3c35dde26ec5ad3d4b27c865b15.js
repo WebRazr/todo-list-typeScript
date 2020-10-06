e7a11a59e2655842b238ec3565b17004bde54fcc
WebRazr <razrsvob@gmail.com>
2020-10-06 15:52:07 +0300

    Updates

---
+++ precache-manifest.7e3ff3c35dde26ec5ad3d4b27c865b15.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "803fd285403a57e8335b105bfc1ddbeb",
+    "url": "/todo-list-typeScript/index.html"
+  },
+  {
+    "revision": "a14c999cd4a384699b2d",
+    "url": "/todo-list-typeScript/static/css/main.6f2b7f43.chunk.css"
+  },
+  {
+    "revision": "5f50268c409d1dbcb795",
+    "url": "/todo-list-typeScript/static/js/2.394db2da.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-list-typeScript/static/js/2.394db2da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a14c999cd4a384699b2d",
+    "url": "/todo-list-typeScript/static/js/main.ba68d389.chunk.js"
+  },
+  {
+    "revision": "706309c20832208e8080",
+    "url": "/todo-list-typeScript/static/js/runtime-main.7154366c.js"
+  },
+  {
+    "revision": "544acaa10bbc65ff41a7344587ab8dc4",
+    "url": "/todo-list-typeScript/static/media/delet.544acaa1.png"
+  },
+  {
+    "revision": "96db4ec436a92751abcf13cf89dbf504",
+    "url": "/todo-list-typeScript/static/media/edit.96db4ec4.png"
+  },
+  {
+    "revision": "06174ccfa8cd14ee789b2236b2f9a0d3",
+    "url": "/todo-list-typeScript/static/media/save.06174ccf.png"
+  }
+]);
